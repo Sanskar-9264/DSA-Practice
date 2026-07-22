b6bed77bdb281a97196450463d8c3cbf7c263581
Sanskar-9264 <sanskarg9264@gmail.com>
2026-07-23 01:34:02 +0530

    Time: 44 ms (81.28%) | Memory: 61.8 MB (38.18%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool isvalid(vector<int>& v ,int m , int middle,int n ){
+        int ball = 1 ;
+        int last_filled_basket = v[0];
+
+        for(int i=1 ;i<n;i++){
+            if(v[i]-last_filled_basket >= middle){
+                ball++;
+                last_filled_basket = v[i];
+            }
+            if(ball == m){
+                return true;
+            }
+
+        }
+        return false;
+    }
+    int maxDistance(vector<int>& pos, int m) {
+        sort(pos.begin(),pos.end());
+        int n = pos.size();
+
+        int st = 1;
+        int end = pos[n-1] - pos[0];
+
+        int ans = -1;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(isvalid(pos,m,mid,n)){
+                ans = mid;
+                st = mid +1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
+};
