17b7f44aaa20b1df4fcd8d6d3a25df87bd9dffb7
Sanskar-9264 <sanskarg9264@gmail.com>
2026-07-25 13:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.85%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,19 +4,13 @@ public:
         int idx = m+n-1 , i = m-1 , j = n-1;
         while(i>= 0 && j>=0){
             if(nums1[i]>=nums2[j]){
-                nums1[idx] = nums1[i];
-                idx-- ;
-                i--;
+                nums1[idx--] = nums1[i--];
             }else{
-                nums1[idx] = nums2[j];
-                idx--;
-                j--;
+                nums1[idx--] = nums2[j--];
             }
         }
         while(j>=0){
-            nums1[idx] = nums2[j];
-            idx--;
-            j--;
+            nums1[idx--] = nums2[j--];
         }
         }
         
