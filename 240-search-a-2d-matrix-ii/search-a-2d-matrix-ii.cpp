5bfcb67602ae82770e08e3a0a09ecc77e71fde11
Sanskar-9264 <sanskarg9264@gmail.com>
2026-07-29 00:56:59 +0530

    Time: 50 ms (72.58%) | Memory: 18.5 MB (93.25%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,7 +3,6 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int m = mat.size() , n = mat[0].size() , r= 0  ;
         int c = n-1;
-
         while(c>= 0 && r <m ){
             if(target == mat[r][c]){
                 return true;
