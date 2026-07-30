8c85aa0e4f9472a574df96cda530f2a3729d5bcb
Sanskar-9264 <sanskarg9264@gmail.com>
2026-07-30 15:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (83.83%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int m = mat.size() , n = mat[0].size();
+        int srow = 0 , scol = 0 , erow = m-1 , ecol = n-1;
+        vector <int> ans;
+        while(srow<=erow && scol<=ecol){
+            // top boundary 
+            for(int j = scol ; j<=ecol ; j++){
+                ans.push_back(mat[srow][j]);
+            }
+            //right boundary
+            for(int j = srow+1 ; j<=erow ; j++){
+                ans.push_back(mat[j][ecol]);
+            }
+            //bottom
+            for(int j = ecol-1 ; j>=scol ; j--){
+                if(srow == erow){
+                    break;
+                }
+                ans.push_back(mat[erow][j]);
+            }
+            //left boundary
+            for(int j = erow-1 ; j>=srow +1 ; j--){
+                if(scol==ecol){
+                    break;
+                }
+                ans.push_back(mat[j][scol]);
+            }
+            srow++ ;
+            scol++ ;
+            erow-- ;
+            ecol--;
+        }return ans ;
+    }
+};
