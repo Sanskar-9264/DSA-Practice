8a9288e0fa57bb3c60e7abb3172ec67ccedb5309
Sanskar-9264 <sanskarg9264@gmail.com>
2026-08-16 21:08:34 +0530

    Time: 32 ms (79.34%) | Memory: 53.6 MB (72.37%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -12,35 +12,22 @@ public:
     void solve(string& s, int idx,
                vector<string>& ans,
                vector<vector<string>>& result) {
-
         if (idx == s.size()) {
             result.push_back(ans);
             return;
         }
         for (int i = idx; i < s.size(); i++) {
-
-            // Only choose palindrome substring
             if (isPalindrome(s, idx, i)) {
-
-                // Choose
                 ans.push_back(s.substr(idx, i - idx + 1));
-
-                // Recursion for remaining string
                 solve(s, i + 1, ans, result);
-
-                // Backtrack
                 ans.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
-
         vector<vector<string>> result;
         vector<string> ans;
-
         solve(s, 0, ans, result);
-
         return result;
     }
 };
