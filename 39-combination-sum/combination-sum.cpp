2b4114bdee3425f12511c16ce64bd564ec7473a1
Sanskar-9264 <sanskarg9264@gmail.com>
2026-09-07 23:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (29.10%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
+    set<vector<int>> s;
+    void combsum(vector<int> &arr, int target, vector<int> &combin,vector<vector<int>> &ans,int i){
 
-    void solve(vector<int>& candidates, int target, int idx,
-               vector<int>& ans, vector<vector<int>>& result) {
-
-        if (target == 0) {
-            result.push_back(ans);
+        if(target==0 ){
+            if(s.find(combin) == s.end()){
+                ans.push_back(combin);
+                s.insert(combin);
+            }         
+            return;
+        }
+        if(i==arr.size() || target<0){
             return;
         }
 
-        if (idx == candidates.size() || target < 0)
-            return;
+        combin.push_back(arr[i]);
+        combsum(arr,target-arr[i],combin,ans,i+1);
+        combsum(arr,target-arr[i],combin,ans,i);
+        combin.pop_back();
+        combsum(arr,target,combin,ans,i+1);
 
-        // Include current element
-        if (candidates[idx] <= target) {
-            ans.push_back(candidates[idx]);
-            
-            solve(candidates, target - candidates[idx],
-                  idx, ans, result);
-
-            // Backtrack
-            ans.pop_back();
-        }
-
-        // Exclude current element
-        solve(candidates, target, idx + 1, ans, result);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-
-        vector<vector<int>> result;
-        vector<int> ans;
-
-        solve(candidates, target, 0, ans, result);
-
-        return result;
+        vector<int>combin;
+        vector<vector<int>>ans;
+        combsum(candidates,target,combin,ans,0);
+        return ans;
     }
 };
